791a3a8f1ed1e34e2fa4acc52200f2e9e02607b3
Kyle Roach <kroach.work@gmail.com>
2016-02-06 16:53:20 -0400

    Add Procfile

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
-
 app.use('/dist', express.static(__dirname + '/dist'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/app', express.static(__dirname + '/app'));
 app.use('/assets', express.static(__dirname + '/assets'));
-
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
