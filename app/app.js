7a335ed75d2a4152bd32bfc6bb874c6a187db261
Kyle Roach <kroach.work@gmail.com>
2016-02-07 06:57:40 -0400

    Added slides information

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ angular.module('markevCouriers', [
 
 .directive('backImg', function(){
     return function(scope, element, attrs){
-        var url = attrs.backImg;
+        var url = attrs['backImg'];
         element.css({
             'background-image': 'url(' + url +')',
             'background-size' : 'cover'
