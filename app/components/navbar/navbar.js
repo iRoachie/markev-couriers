49c7d4b3378b8a357f21b64537e0dae117ffec95
Kyle Roach <kroach.work@gmail.com>
2016-02-10 19:41:40 -0400

    Small fixes in css for responsive

--- app/components/navbar/navbar.js
+++ app/components/navbar/navbar.js
@@ -5,27 +5,24 @@ angular
   .component('navbar', {
     templateUrl: '/app/components/navbar/navbar.html',
     controller: function (smoothScroll, $state, $timeout) {
-      var vm = this;
+      this.scroll = scroll;
 
-      vm.scroll = function scroll(id) {
+      function scroll(id) {
         if ($state.current.name === 'home') {
           var element = document.getElementById(id);
-
-          console.log(element);
           var options = {
             duration: 600,
             offset: 60
           };
           smoothScroll(element, options);
         } else {
-          $state.go('home').then(function() {
-            $timeout(function() {
-              vm.scroll(id);
+          $state.go('home').then(function () {
+            $timeout(function () {
+              scroll(id);
             }, 300)
           })
         }
       }
-    },
-    controllerAs: "vm"
+    }
   });
 
