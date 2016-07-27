7937b85fba21a1651c17aa312dd0ced84094b964
Kyle Roach <kroach.work@gmail.com>
2016-07-26 21:30:18 -0400

    Fixed bug with email not sending

--- app/components/sendEmail.factory.js
+++ app/components/sendEmail.factory.js
@@ -3,7 +3,7 @@
 angular
   .module('markevCouriers')
   .factory('EmailService', function($http, $location) {
-    var host = $location.$$host !== 'localhost' ? 'https://markev-couriers.herokuapp.com' : 'http://localhost:5000';
+    var host = $location.$$host !== 'localhost' ? 'http://www.markevcouriers.com' : 'http://localhost:5000';
 
     return {
       sendEmail: function(info) {
