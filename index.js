6499f4eeaad59f83b8450f8a5c0448793770d79a
Kyle Roach <kroach.work@gmail.com>
2017-01-23 23:01:13 -0400

    Added compression

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
+const compression = require('compression')
 const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
+app.use(compression())
 app.use(bodyParser.json());
-app.use(cors());
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('_site'))
